a5c32e173b59f9af0986658e76090d3f0cde3cf0
taeminaa <mehrvarzmina7@gmail.com>
2026-04-15 15:29:11 +0200

    fixed OOM issue

--- src/objectives/explanation/evaluation/metrics.py
+++ src/objectives/explanation/evaluation/metrics.py
@@ -11,7 +11,7 @@ from utils.utils import normalize_cam
 # SSIM
 # ==============================
 def compute_ssim_batch(a, b, device):
-    ssim_metric = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
+    ssim_metric = StructuralSimilarityIndexMeasure(data_range=1.0, reduction="none" ).to(device)
     a = a.unsqueeze(1)
     b = b.unsqueeze(1)
     return ssim_metric(a, b).detach().cpu().numpy()
@@ -39,21 +39,20 @@ def evaluate_explanations(model, clean_model, dataloader, attack, target_fn, dev
         # ===== CLEAN =====
         logits_clean = clean_model(images)
         cams_ref = cam_clean(logits_clean, labels, create_graph=False)
+        cams_ref = normalize_cam(cams_ref).detach()
 
         logits_model = model(images)
-        cams_model = cam_model(logits_model, labels, create_graph=False)
-
-        cams_ref = normalize_cam(cams_ref)
-        cams_model = normalize_cam(cams_model)
+        cams_model = cam_model(logits_model, labels, create_graph=False) 
+        cams_model = normalize_cam(cams_model).detach()
 
         # metrics clean
-        mse_clean.extend(((cams_model - cams_ref)**2).mean(dim=(1,2)).detach().cpu().numpy())
+        mse_clean.extend(((cams_model - cams_ref)**2).mean(dim=(1,2)).cpu().numpy())
 
         cos_clean.extend(F.cosine_similarity(
             cams_model.view(cams_model.size(0), -1),
             cams_ref.view(cams_ref.size(0), -1),
             dim=1
-        ).detach().cpu().numpy())
+        ).cpu().numpy())
 
         ssim_clean.extend(compute_ssim_batch(cams_model, cams_ref, device))
 
@@ -62,30 +61,33 @@ def evaluate_explanations(model, clean_model, dataloader, attack, target_fn, dev
 
         logits_trig = model(images_trig)
         cams_trig = cam_model(logits_trig, labels, create_graph=False)
-        cams_trig = normalize_cam(cams_trig)
+        cams_trig = normalize_cam(cams_trig).detach()
 
         cam_h, cam_w = cams_trig.shape[-2:]
         target = target_fn(cam_h, cam_w, cams_trig.size(0))
         target = normalize_cam(target)
 
         # metrics trigger
-        mse_trigger.extend(((cams_trig - target)**2).mean(dim=(1,2)).detach().cpu().numpy())
+        mse_trigger.extend(((cams_trig - target)**2).mean(dim=(1,2)).cpu().numpy())
 
         cos_tt.extend(F.cosine_similarity(
             cams_trig.view(cams_trig.size(0), -1),
             target.view(target.size(0), -1),
             dim=1
-        ).detach().cpu().numpy())
+        ).cpu().numpy())
 
         cos_tc.extend(F.cosine_similarity(
             cams_trig.view(cams_trig.size(0), -1),
             cams_ref.view(cams_ref.size(0), -1),
             dim=1
-        ).detach().cpu().numpy())
+        ).cpu().numpy())
 
         ssim_trigger.extend(compute_ssim_batch(cams_trig, target, device))
+
+        #memory cleanup
         del logits_clean, logits_model, logits_trig
         del cams_ref, cams_model, cams_trig
+        torch.cuda.empty_cache()
  
     cam_model.remove()
     cam_clean.remove() 
